680fba3b27e3e36a6f695ffefb43a3d1f4d7326d
Netanel <mordakhay.netanel@gmail.com>
2024-12-20 16:20:28 +0200

    Basic Nav bar (genres list)

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,26 @@
+import { Group, Image, List, Text } from "@mantine/core";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageURL from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List listStyleType="none">
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <List.Item key={genre.id} my={2}>
+          <Group>
+            <Image
+              h="32px"
+              w="32px"
+              radius={16}
+              src={getCroppedImageURL(genre.image_background)}
+            />
+            <Text size="lg">{genre.name}</Text>
+          </Group>
+        </List.Item>
       ))}
-    </ul>
+    </List>
   );
 };
 
