8d0f66cc0bb75d7a579e243537d263be46ef191f
Netanel <mordakhay.netanel@gmail.com>
2024-12-21 19:51:26 +0200

    metacritic score badge fixed if none

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,6 +5,10 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
+  // No metacritic score available
+  if (!score) return null;
+
+  // Return metacritic score badge based on score
   let color =
     score > 85 ? "green" : score > 75 ? "lime" : score > 60 ? "yellow" : "red";
   return <Badge color={color}>{score}</Badge>;
