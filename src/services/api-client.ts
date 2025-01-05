6a4d03d65789c65d7b0d361b0c02458196be8cc3
Netanel <mordakhay.netanel@gmail.com>
2025-01-05 15:04:56 +0200

    Refactored api-client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,29 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "7c64a7b6ab19416ea5f7853912c5fbd3",
   },
 });
 
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
+
+export default APIClient;
