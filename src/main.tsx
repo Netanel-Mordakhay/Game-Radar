97d5c58218b5f1a6265c89a402b8c7abd2278600
Netanel <mordakhay.netanel@gmail.com>
2025-01-09 20:49:45 +0200

    main deleted redundent import

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "@mantine/core/styles.css";
 import "./index.css";
-import App from "./App.tsx";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
 
