814ced6e6f2bf0df2d00fba7ee7e78ef716ec59f
Netanel <mordakhay.netanel@gmail.com>
2025-01-10 11:59:10 +0200

    Navbar fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { Group, Image, Title } from "@mantine/core";
 import logo from "../assets/logo.webp";
 import ColorActionSwitch from "./ColorActionSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Group w="100%" justify="space-between">
-      <Image src={logo} w="40px" h="40px" />
+      <Link to="/">
+        <Image src={logo} w="40px" h="40px" />
+      </Link>
       <Title visibleFrom="sm" order={2}>
         Game Radar
       </Title>
